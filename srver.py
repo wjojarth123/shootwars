33d9b2baf57e4b24d0c3bc98331d958964220aa5
wjojarth123 <wjojarth@gmail.com>
2018-12-31 15:58:30 -0800

    fixed bullet sending

--- srver.py
+++ srver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import math
 import socket
+import time
 import pygame
 import json
 pygame.init()
@@ -15,16 +16,19 @@ x=0
 y=0
 done=False
 bulletlist=[]
+ebulletlist=[]
+cooldown=1
+lasttime=time.time()
 def readData(opponent):
     global bulletlist
     ox=int(opponent[:opponent.find(",")])
     oy=int(opponent[(opponent.find(",")+1):(opponent.find("-"))])
-    
+    ebulletlist=[]
     ebl=opponent[(opponent.find("-")+1):]
     ebl=json.loads(ebl)
     for i in range(len(ebl)):
        ebl[i]=tuple(ebl[i])
-       bulletlist.append(ebl[i])
+       ebulletlist.append(ebl[i])
     print(ebl)
     return (ox,oy)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -38,16 +42,26 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         conn = s
     print(PORT,conn)
     while not done:
+        hasShot=0
+        currenttime=time.time()
+        cooldown-=(currenttime-lasttime)
+        lasttime=currenttime
+        print(cooldown)
         screen.fill((0,0,0))
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 done=True
-            if event.type==pygame.MOUSEBUTTONDOWN:
-                
+            if event.type==pygame.MOUSEBUTTONDOWN and cooldown < 0 and hasShot==0: 
                 bulletlist.append((x,y,1,1))
+                print("why doesn't bla bla work. this time non")
+                cooldown=1
+                hasShot=1
         for i in range(len(bulletlist)):
             pygame.draw.rect(screen,(255,255,255),pygame.Rect(bulletlist[i][0],bulletlist[i][1],10,10))
             bulletlist[i]=(bulletlist[i][0]+bulletlist[i][2],bulletlist[i][1]+bulletlist[i][3],1,1)
+        for i in range(len(ebulletlist)):
+            pygame.draw.rect(screen,(255,255,255),pygame.Rect(ebulletlist[i][0],ebulletlist[i][1],10,10))
+            ebulletlist[i]=(ebulletlist[i][0]+ebulletlist[i][2],ebulletlist[i][1]+ebulletlist[i][3],1,1)
         pressed=pygame.key.get_pressed()
         if pressed[pygame.K_UP] and y>0:
             y-=3
@@ -69,13 +83,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             conn.sendall(bytes(str(x)+","+str(y)+"-"+json.dumps(bulletlist),'utf-8'))
         if not isServer:
             bls = ''
-            for i in range(len(bulletlist)):
-                 
-
             conn.sendall(bytes(str(x)+","+str(y)+"-"+json.dumps(bulletlist),'utf-8'))
             data = ''
             data = conn.recv(1024)
             opponent=data.decode('utf-8')
             epos=readData(opponent)
             pygame.draw.rect(screen,(0,0,255),pygame.Rect(epos[0],epos[1],40,40))
+            cd-=1
         pygame.display.flip()
